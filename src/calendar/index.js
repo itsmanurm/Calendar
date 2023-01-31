3afabeb43f203f917b3796bad41686f89e8731a4
Manuel <mrm291201@gmail.com>
2023-01-31 18:18:35 -0300

    Error en calendarSlice

---
+++ src/calendar/index.js
@@ -0,0 +1,9 @@
+
+
+export * from './pages/CalendarPage';
+
+export * from './components/NavBar';
+
+export * from './components/CalendarEvent';
+
+export * from './components/CalendarModal';
---
+++ src/store/calendar/calendarSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { addHours } from 'date-fns';
+
+const tempEvent =   {
+    _id: new Date().getTime(),
+    title: 'Cumpleaños del Jefe',
+    notes: 'Hay que comprar el pastel',
+    start: new Date(),
+    end: addHours( new Date(), 2 ),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Fernando'
+    }
+};
+
+
+export const calendarSlice = createSlice({
+    name: 'calendar',
+    initialState: {
+        events: [
+            tempEvent
+        ],
+        activeEvent: null
+    },
+    reducers: {
+        
+    }
+});
+
+
+export const {  } = calendarSlice.actions;
